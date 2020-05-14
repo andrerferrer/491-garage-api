31cd9c73f46845e716cedf8d04485a84f98234d3
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-14 16:32:26 +0200

    Add pseudo code to the challenge

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 // TODO: Build an awesome garage!
+
+// To get all cars
+// 1. Send an AJAX request to the API to fetch all cars
+// 2. For each car
+//    3. Insert a new row in the cars table
+
+// To add a new car
+// 1. Add an event listener on the form
+// 2. Prevent default behavior when we submit it
+// 3. Retrieve data entered by the user in the inputs
+// 4. Send a POST ajax request to the API
+// 5. Refresh cars list
